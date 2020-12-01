7de60dfc837aa5cd63b7af42d463982de5813c70
Salil Chhetri <salilchetri@gmail.com>
2020-12-01 11:40:46 +0530

    wip home

---
+++ components/home/hero/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Hero.scss';
+
+export default function Hero() {
+    return (
+        <div id="hero">
+            <h1>Hello World</h1>
+            <div className="inner">
+            </div>
+        </div>
+    )
+}
